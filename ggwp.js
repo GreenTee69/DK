81cffd4e174ebd7aadd7559afbb48bc3d0fcdb44
GreenTee69 <GreenTee69@users.noreply.github.com>
2019-08-05 20:19:59 +0200

    Create ggwp.js

---
+++ ggwp.js
@@ -0,0 +1,10 @@
+/* UTILITIES */
+function l(s) {console.log(s);}
+function qs(s) {return document.querySelector(s);}
+function qsA(s) {return document.querySelectorAll(s);}
+function cr(s) {return document.createElement(s);}
+function isOK(q) {return q && q != null && q != undefined ? true : false;}
+function insertBef(newNode, referenceNode) {referenceNode.parentNode.insertBefore(newNode, referenceNode.previousSibling);}
+function insertAf(newNode, referenceNode) {referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);}
+
+l("asd");
